358f1a444379a40cf32d4d1a2cfac24a1a9d7598
mateuszk <mat.kozlowski99@gmail.com>
2025-01-22 14:33:59 +0100

    ekf1 2d

--- Core/Src/ekf_1.c
+++ Core/Src/ekf_1.c
@@ -19,10 +19,12 @@
 /*==============*/
 /* Debug switch */
 /*==============*/
-#define SENSOR_DATA
+#define TEST_DATA
 
 // time step duration
 eekf_value dT = 0.01;
+
+eekf_value dT_2 = 1;
 // process noise standard deviation
 eekf_value s_w = 0.1;
 // measurement noise standard deviation
@@ -50,17 +52,35 @@ void UART2_SendString_ekf_1(char* s)
 eekf_return transition(eekf_mat* xp, eekf_mat* Jf, eekf_mat const *x,
         eekf_mat const *u, void* userData)
 {
-	EEKF_DECL_MAT_INIT(xu, 2, 1, 0);
-	EEKF_DECL_MAT_INIT(B, 2, 1, dT * dT / 2, dT);
+	EEKF_DECL_MAT_INIT(xu, 4, 1, 0);
+	EEKF_DECL_MAT_INIT(B, 4, 2, 0,0,0,0,0,0,0,0);
 	
     // the Jacobian of transition() at x
     *EEKF_MAT_EL(*Jf, 0, 0) = 1;
     *EEKF_MAT_EL(*Jf, 1, 0) = 0;
-    *EEKF_MAT_EL(*Jf, 0, 1) = dT;
+    *EEKF_MAT_EL(*Jf, 2, 0) = 0;
+    *EEKF_MAT_EL(*Jf, 3, 0) = 0;
+    *EEKF_MAT_EL(*Jf, 0, 1) = 0;
     *EEKF_MAT_EL(*Jf, 1, 1) = 1;
+    *EEKF_MAT_EL(*Jf, 2, 1) = 0;
+    *EEKF_MAT_EL(*Jf, 3, 1) = 0;
+    *EEKF_MAT_EL(*Jf, 0, 2) = dT;
+    *EEKF_MAT_EL(*Jf, 1, 2) = 0;
+    *EEKF_MAT_EL(*Jf, 2, 2) = 1;
+    *EEKF_MAT_EL(*Jf, 3, 2) = 0;
+    *EEKF_MAT_EL(*Jf, 0, 3) = 0;
+    *EEKF_MAT_EL(*Jf, 1, 3) = dT;
+    *EEKF_MAT_EL(*Jf, 2, 3) = 0;
+    *EEKF_MAT_EL(*Jf, 3, 3) = 1;
 
 	*EEKF_MAT_EL(B, 0, 0) = dT * dT / 2;
-	*EEKF_MAT_EL(B, 1, 0) = dT;
+	*EEKF_MAT_EL(B, 1, 0) = 0;
+    *EEKF_MAT_EL(B, 2, 0) = dT;
+	*EEKF_MAT_EL(B, 3, 0) = 0;
+    *EEKF_MAT_EL(B, 0, 1) = 0;
+	*EEKF_MAT_EL(B, 1, 1) = dT * dT / 2;
+    *EEKF_MAT_EL(B, 2, 1) = 0;
+	*EEKF_MAT_EL(B, 3, 1) = dT;
 	
     // predict state from current state
     if (NULL == eekf_mat_add(xp, eekf_mat_mul(xp, Jf, x), eekf_mat_mul(&xu, &B, u)))
@@ -77,10 +97,13 @@ eekf_return measurement(eekf_mat* zp, eekf_mat* Jh, eekf_mat const *x,
 {
     // the Jacobian of measurement() at x
     *EEKF_MAT_EL(*Jh, 0, 0) = 1;
+    *EEKF_MAT_EL(*Jh, 1, 0) = 0;
     *EEKF_MAT_EL(*Jh, 0, 1) = 0;
+    *EEKF_MAT_EL(*Jh, 1, 1) = 1;
 
     // compute the measurement from state x
     *EEKF_MAT_EL(*zp, 0, 0) = *EEKF_MAT_EL(*x, 0, 0);
+    *EEKF_MAT_EL(*zp, 1, 0) = *EEKF_MAT_EL(*x, 1, 0);
 
     return eEekfReturnOk;
 }
@@ -91,23 +114,39 @@ void run_ekf_1(void){
 
     // state of the filter
     #ifdef TEST_DATA
-    EEKF_DECL_MAT_INIT(x, 2, 1,data_gps[0][0], 0);
+    EEKF_DECL_MAT_INIT(x, 4, 1, data_gps[0][0], data_gps[0][1], 0, 0);
     #endif
-    EEKF_DECL_MAT_INIT(P, 2, 2, 
-		pow(s_w, 2) * pow(dT, 4) / 4, pow(s_w, 2) * pow(dT, 3) / 2,
-		pow(s_w, 2) * pow(dT, 3) / 2, pow(s_w, 2) * pow(dT, 2));
-    // input and process noise variables            
-    EEKF_DECL_MAT_INIT(u, 1, 1, 0);
+    EEKF_DECL_MAT_INIT(P, 4, 4, 
+		pow(s_w, 2), 0, 0, 0,
+		0, pow(s_w, 2), 0, 0,
+        0, 0, pow(s_w, 2), 0,
+        0, 0, 0, pow(s_w, 2));
 
-    /* Tak wynika z Kalman Filtering, Dan Simon*/
-    EEKF_DECL_MAT_INIT(Q, 2, 2, 
-		pow(s_w, 2) * pow(dT, 4) / 4, pow(s_w, 2) * pow(dT, 3) / 2,
-		pow(s_w, 2) * pow(dT, 3) / 2, pow(s_w, 2) * pow(dT, 2));
+
+    // input and process noise variables            
+    EEKF_DECL_MAT_INIT(u, 2, 1, 0, 0);
+
+/*
+    EEKF_DECL_MAT_INIT(Q, 4, 4, 
+		pow(s_w, 2) * pow(dT, 4) / 4, 0, pow(s_w, 2) * pow(dT, 3) / 2, 0,
+		0,  pow(s_w, 2) * pow(dT, 4) / 4, 0, pow(s_w, 2) * pow(dT, 3) / 2,
+        pow(s_w, 2) * pow(dT, 3) / 2, 0, pow(s_w, 2) * pow(dT, 2), 0,
+        0, 0, 0, pow(s_w, 2) * pow(dT, 2));
+*/
+
+    EEKF_DECL_MAT_INIT(Q, 4, 4, 
+		pow(s_w, 2) * pow(dT, 2), 0, 0, 0,
+		0, pow(s_w, 2) * pow(dT, 2), 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0);
+        
     // measurement and measurement noise variables
-    EEKF_DECL_MAT_INIT(z, 1, 1, 0);
-    EEKF_DECL_MAT_INIT(R, 1, 1, s_z * s_z);
+    EEKF_DECL_MAT_INIT(z, 2, 1, 0, 0);
+    EEKF_DECL_MAT_INIT(R, 2, 2, pow(s_z, 2) * pow(dT_2, 2), 0,
+                                0, pow(s_z, 2) * pow(dT_2, 2));
 
     eekf_value x_predicted = 0; 
+    eekf_value y_predicted = 0; 
     
     #ifdef SENSOR_DATA
     MPU9250_begin();
@@ -156,13 +195,19 @@ void run_ekf_1(void){
         for(uint16_t k = 0; k < 4; k++){
             for(uint8_t i = 0; i < imu_sampling_rate/gps_sampling_rate; i++){
                 *EEKF_MAT_EL(u, 0, 0) = data_imu[i + k*(imu_sampling_rate/gps_sampling_rate)][3];
+                *EEKF_MAT_EL(u, 1, 0) = data_imu[i + k*(imu_sampling_rate/gps_sampling_rate)][4];
                 eekf_predict(&ctx, &u, &Q);   
             }
             *EEKF_MAT_EL(z, 0, 0) = data_gps[k][0];
+            *EEKF_MAT_EL(z, 1, 0) = data_gps[k][1];
             x_predicted = *EEKF_MAT_EL(*ctx.x, 0, 0);
+            y_predicted = *EEKF_MAT_EL(*ctx.x, 1, 0);
             eekf_correct(&ctx, &z, &R);		
             memset(buffer_ekf_1, 0, 128);
-            sprintf((char*)buffer_ekf_1, "INS: %.4f   GPS: %.4f   KAL: %.4f\n\r", x_predicted, data_gps[k][0], *EEKF_MAT_EL(*ctx.x, 0, 0));
+            sprintf((char*)buffer_ekf_1, "X: INS: %.5f   GPS: %.5f   KAL: %.5f\n\r", x_predicted, data_gps[k][0], *EEKF_MAT_EL(*ctx.x, 0, 0));
+            UART2_SendString_ekf_1((char*)buffer_ekf_1);
+            memset(buffer_ekf_1, 0, 128);
+            sprintf((char*)buffer_ekf_1, "Y: INS: %.5f   GPS: %.5f   KAL: %.5f\n\r", y_predicted, data_gps[k][1], *EEKF_MAT_EL(*ctx.x, 1, 0));
             UART2_SendString_ekf_1((char*)buffer_ekf_1);
         }		
         memset(buffer_ekf_1, 0, 128);
